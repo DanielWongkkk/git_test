6e19e5780e05c34c1ed61468528e9be12433b3ef
DanielWongkkk <49525297+DanielWongkkk@users.noreply.github.com>
2021-07-13 17:42:06 +0800

    add

--- src/main/java/com/kris/git/GitTest.java
+++ src/main/java/com/kris/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("new");
+        System.out.println("true");
+        
     }
 }
